14bc5505873eb4637b84db349926fd79ca919221
zzhang87 <thomas.zz.zhang@gmail.com>
2018-03-07 21:00:50 -0500

    add early_stop

--- early_stop.py
+++ early_stop.py
@@ -7,8 +7,17 @@ import tensorflow as tf
 def main():
 	ap = argparse.ArgumentParser()
 	ap.add_argument('--logdir', type = str, help = 'Directory to the model logs')
+	ap.add_argument('--patience', type = int, help = 'Patience parameter')
 
 	args = ap.parse_args()
+
+	if os.path.exists(os.path.join(args.logdir, 'val_loss.txt')):
+		losses = np.loadtxt(os.path.join(args.logdir, 'val_loss.txt'))
+		if losses.ndim < 1:
+			losses = np.expand_dims(losses, 0)
+		losses = list(losses)
+	else:
+		losses = []
 
 	files = os.listdir(args.logdir)
 	events = [f for f in files if 'event' in f]
@@ -21,7 +30,28 @@ def main():
 	for e in tf.train.summary_iterator(event):
 		for v in e.summary.value:
 			if 'Loss' in v.tag:
-				print('{}: {}'.format(v.tag, v.simple_value))
+				losses.append(v.simple_value)
+
+	arr = np.array(losses, dtype = np.float32)
+	np.savetxt(os.path.join(args.logdir, 'val_loss.txt'), arr)
+
+	diff = np.diff(arr)
+
+	if len(arr) <= args.patience:
+		print('continue')
+
+	else:
+		stop = True
+
+		for i in diff[-args.patience:]:
+			stop &= i > 0
+
+		if stop:
+			print('stop')
+
+		else:
+			print('continue')
+
 
 if __name__ == "__main__":
 	main()
--- finetune_resnet.sh
+++ finetune_resnet.sh
@@ -15,16 +15,19 @@ MODEL_NAME=resnet_v2_50
 PRETRAINED_CHECKPOINT_DIR=/home/zzhang52/Libs/tensorflow/checkpoints/${MODEL_NAME}
 
 # Where the training (fine-tuned) checkpoint and logs will be saved to.
-TRAIN_DIR=/home/zzhang52/Insight/CXR/new_train
+TRAIN_DIR=/home/zzhang52/Insight/CXR/logs
 
 # Where the dataset is saved to.
 DATASET_DIR=/home/zzhang52/Insight/CXR/test
 
-PARTITION_ID=2
+PARTITION_ID=1
 
 PARTITION_NUM=2
 
-# Fine-tune only the new layers for 300 steps.
+NUM_EPOCHS=2
+
+PATIENCE=2
+
 python train_image_classifier.py \
   --train_dir=${TRAIN_DIR} \
   --dataset_split_name=train \
@@ -36,7 +39,31 @@ python train_image_classifier.py \
   --checkpoint_path=${PRETRAINED_CHECKPOINT_DIR}/${MODEL_NAME}.ckpt \
   --checkpoint_exclude_scopes=${MODEL_NAME}/logits \
   --trainable_scopes=${MODEL_NAME}/logits \
-  --num_epochs=10 \
+  --num_epochs=1 \
+  --batch_size=32 \
+  --learning_rate=0.01 \
+  --save_interval_secs=600 \
+  --save_summaries_secs=60 \
+  --log_every_n_steps=100 \
+  --optimizer=rmsprop \
+  --weight_decay=0.00004
+
+
+for epoch in `seq 1 $NUM_EPOCHS`
+do
+# Fine-tune only the new layers.
+python train_image_classifier.py \
+  --train_dir=${TRAIN_DIR} \
+  --dataset_split_name=train \
+  --partition_id=${PARTITION_ID} \
+  --partition_num=${PARTITION_NUM} \
+  --dataset_dir=${DATASET_DIR} \
+  --model_name=${MODEL_NAME} \
+  --preprocessing_name=inception \
+  --checkpoint_path=${TRAIN_DIR} \
+  --checkpoint_exclude_scopes=${MODEL_NAME}/logits \
+  --trainable_scopes=${MODEL_NAME}/logits \
+  --num_epochs=1 \
   --batch_size=32 \
   --learning_rate=0.01 \
   --save_interval_secs=600 \
@@ -55,9 +82,20 @@ python eval_image_classifier.py \
   --dataset_dir=${DATASET_DIR} \
   --model_name=${MODEL_NAME} \
   --preprocessing_name=inception \
-  --eval_image_size=299
+  --eval_image_size=299 \
+  --batch_size=64
 
-# Fine-tune all the layers for 100 steps.
+# Check flag.
+FLAG="$(python early_stop.py --logdir ${TRAIN_DIR} --patienc $PATIENCE)"
+if [ $FLAG = stop ]
+  then
+  break
+fi
+done
+
+for epoch in `seq 1 $NUM_EPOCHS`
+do
+# Fine-tune all the layers.
 python train_image_classifier.py \
   --train_dir=${TRAIN_DIR}/all \
   --dataset_split_name=train \
@@ -86,5 +124,13 @@ python eval_image_classifier.py \
   --dataset_dir=${DATASET_DIR} \
   --model_name=${MODEL_NAME} \
   --preprocessing_name=inception \
-  --eval_image_size=299
+  --eval_image_size=299 \
+  --batch_size=64
 
+# Check flag.
+FLAG="$(python early_stop.py --logdir ${TRAIN_DIR}/all --patienc $PATIENCE)"
+if [ $FLAG = stop ]
+  then
+  break
+fi
+done
