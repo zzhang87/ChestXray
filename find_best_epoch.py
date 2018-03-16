b5f78bfaee51be12eec44397c7d3f676951b39df
zzhang87 <thomas.zz.zhang@gmail.com>
2018-03-16 19:04:15 -0400

    fixed bug

--- find_best_epoch.py
+++ find_best_epoch.py
@@ -10,23 +10,23 @@ def main():
 
 	file_list = [x for x in os.listdir(args.dir) if 'test' in x and 'auc' in x]
 
-	best = 1.
+	best = 0.
 	best_epoch = None
 
 	for file in file_list:
-		epoch = file.replace('-', '.').split('.')
-		epoch = int(epoch[1])
+		epoch = file.replace('_', '.').split('.')
+		epoch = int(epoch[-2])
 
 		with open(os.path.join(args.dir, file), 'r') as f:
 			auc_scores = json.load(f)
 
 		mean_auc = np.mean(np.array(auc_scores.values()))
-		if mean_auc < best:
+		if mean_auc > best:
 			best_epoch = epoch
 			best = mean_auc
 
 
-	print(best_epoch)
+	print("Epoch with highest mean AUROC: {}".format(best_epoch))
 
 if __name__ == "__main__":
 	main()
